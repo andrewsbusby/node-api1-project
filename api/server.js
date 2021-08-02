cba03bb2a642036d6052017eda144c9cd1a0b7bc
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-02 14:15:53 -0600

    findbyid and insert are working

--- api/server.js
+++ api/server.js
@@ -6,13 +6,51 @@ const server = express();
 
 server.use(express.json());
 
+// FIND ALL USERS
 server.get('/api/users', (req, res) => {
     User.find()
         .then(users => {
             res.json(users)
         })
         .catch(err => {
-            res.status(500).json({message: "There was an error while saving the user to the database"})
+            res.status(500).json({message: "The users information could not be retrieved"})
+        })
+})
+
+//FIND USER BY ID
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    User.findById(id)
+        .then(user => {
+            if(!user) {
+                res.status(404).json({message: "The user with the specified ID does not exist"})
+            }
+            res.json(user)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The user information could not be retrieved"
+            })
+        })
+})
+
+//CREATE NEW USER   
+server.post('/api/users', (req, res) => {
+    const { name, bio } = req.body
+    User.insert({ name, bio })
+        .then(newUser => {
+            if(!newUser) {
+                res.status(400).json({
+                    message: "Please provide name and bio for the user"
+                })
+            } else {
+                res.status(201).json(newUser);
+            }
+        })
+        .catch(err =>{
+            res.status(500).json({
+                message: "There was an error while saving the user to the database"
+            })
         })
 })
 
