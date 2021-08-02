1e33fd1029f180c981bfe03fbe1e84583e31867d
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-02 13:55:40 -0600

    find all users working

--- api/server.js
+++ api/server.js
@@ -1,5 +1,19 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
+const User = require('./users/model');
+
 const server = express();
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({message: "There was an error while saving the user to the database"})
+        })
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
